40c6879719e09042da400495afc4d0e4e39d29ab
Pierre Durand <pierredurand@gmail.com>
2024-08-11 09:55:34 +0200

    remove decimal value in AllocsPerRun() error message

--- alloc.go
+++ alloc.go
@@ -14,7 +14,7 @@ func AllocsPerRun(tb testing.TB, runs int, f func(), allocs float64, opts ...Opt
 		Fail(
 			tb,
 			"allocs_per_run",
-			fmt.Sprintf("unexpected allocs:\nexpected = %f\nactual = %f", allocs, a),
+			fmt.Sprintf("unexpected allocs:\nexpected = %g\nactual = %g", allocs, a),
 			opts...,
 		)
 	}
