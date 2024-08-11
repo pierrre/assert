3186c2452109c26d22322f1f195fd9c3694d92e5
Pierre Durand <pierredurand@gmail.com>
2024-08-11 03:51:39 +0200

    use reflectutil.TypeFullName() instead of TypeFullName()
    It will be removed.

--- type.go
+++ type.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/pierrre/go-libs/reflectutil"
@@ -23,5 +24,5 @@ func Type[T any](tb testing.TB, v any, opts ...Option) (T, bool) {
 }
 
 func typeName[T any]() string {
-	return reflectutil.TypeFullNameFor[T]()
+	return reflectutil.TypeFullName(reflect.TypeFor[T]())
 }
