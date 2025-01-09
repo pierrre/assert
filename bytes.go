9aadbc5a45ca386175230060463b0639110da52b
Pierre Durand <pierredurand@gmail.com>
2025-01-09 16:33:49 +0100

    add BytesContains|BytesNotContains

--- bytes.go
+++ bytes.go
@@ -41,3 +41,39 @@ func BytesNotEqual(tb testing.TB, b1, b2 []byte, opts ...Option) bool {
 	}
 	return ok
 }
+
+// BytesContains asserts that b contains subslice.
+// It uses [bytes.Contains] to check if subslice is contained in b.
+//
+//nolint:thelper // It's called below.
+func BytesContains(tb testing.TB, b, subslice []byte, opts ...Option) bool {
+	ok := bytes.Contains(b, subslice)
+	if !ok {
+		tb.Helper()
+		Fail(
+			tb,
+			"bytes_contains",
+			fmt.Sprintf("not contains:\nb = %s\nsubslice = %s", ValueStringer(b), ValueStringer(subslice)),
+			opts...,
+		)
+	}
+	return ok
+}
+
+// BytesNotContains asserts that b does not contain subslice.
+// It uses [bytes.Contains] to check if subslice is contained in b.
+//
+//nolint:thelper // It's called below.
+func BytesNotContains(tb testing.TB, b, subslice []byte, opts ...Option) bool {
+	ok := !bytes.Contains(b, subslice)
+	if !ok {
+		tb.Helper()
+		Fail(
+			tb,
+			"bytes_not_contains",
+			fmt.Sprintf("contains:\nb = %s\nsubslice = %s", ValueStringer(b), ValueStringer(subslice)),
+			opts...,
+		)
+	}
+	return ok
+}
--- bytes_test.go
+++ bytes_test.go
@@ -28,3 +28,25 @@ func TestBytesNotEqualFail(t *testing.T) {
 	ok := BytesNotEqual(t, []byte("abc"), []byte("abc"), Report(report))
 	False(t, ok)
 }
+
+func TestBytesContains(t *testing.T) {
+	ok := BytesContains(t, []byte("abc"), []byte("b"))
+	True(t, ok)
+}
+
+func TestBytesContainsFail(t *testing.T) {
+	report := asserttest.NewReportAuto(t)
+	ok := BytesContains(t, []byte("abc"), []byte("d"), Report(report))
+	False(t, ok)
+}
+
+func TestBytesNotContains(t *testing.T) {
+	ok := BytesNotContains(t, []byte("abc"), []byte("d"))
+	True(t, ok)
+}
+
+func TestBytesNotContainsFail(t *testing.T) {
+	report := asserttest.NewReportAuto(t)
+	ok := BytesNotContains(t, []byte("abc"), []byte("b"), Report(report))
+	False(t, ok)
+}
