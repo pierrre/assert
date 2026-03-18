844733bb7e368983371807b55f70b966c4b393d0
Pierre Durand <pierredurand@gmail.com>
2026-03-18 20:39:51 +0100

    rename SignedAndFloat to Signed

--- signed.go
+++ signed.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 )
 
-// SignedAndFloat is a constraint that requires a type to be signed or float.
-type SignedAndFloat interface {
+// Signed represents all signed numeric types.
+type Signed interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~float32 | ~float64
 }
 
 // Positive asserts that the value is positive.
 //
 //nolint:thelper // It's called below.
-func Positive[T SignedAndFloat](tb testing.TB, v T, opts ...Option) bool {
+func Positive[T Signed](tb testing.TB, v T, opts ...Option) bool {
 	ok := v > 0
 	if !ok {
 		tb.Helper()
@@ -30,7 +30,7 @@ func Positive[T SignedAndFloat](tb testing.TB, v T, opts ...Option) bool {
 // Negative asserts that the value is negative.
 //
 //nolint:thelper // It's called below.
-func Negative[T SignedAndFloat](tb testing.TB, v T, opts ...Option) bool {
+func Negative[T Signed](tb testing.TB, v T, opts ...Option) bool {
 	ok := v < 0
 	if !ok {
 		tb.Helper()
